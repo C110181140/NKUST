0f6c9b510dd7e20f62dedf8ae280d11885744ee4
C110181140 <byron920521>
2021-11-23 16:23:40 +0800

    updata

--- Comment.java
+++ Comment.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Comment {
     public static void main(String[] args) {
         // This is comment
@@ -6,12 +5,4 @@ public class Comment {
         // 這是註解
     }
 }
-=======
-public class Comment {
-    public static void main(String[] args) {
-        // This is comment
-        // Hi Hi Hi
-        // 這是註解
-    }
-}
->>>>>>> origin/main
+
