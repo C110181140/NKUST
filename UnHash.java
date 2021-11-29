8fb151192d326824c98c953821f3d2ad03d67d7b
C110181140 <C110181140@nkust.edu.tw>
2021-11-29 20:24:25 +0800

    updata

--- UnHash.java
+++ UnHash.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 public class UnHash {
     public static void main (String[] args) {
         System.out.println(revHash(6933552791181934L));
@@ -7,19 +9,28 @@ public class UnHash {
     public static String letters = "cdefghijlmnoqstuvxz";
 
     public static long revHash(long hash) {
+        ArrayList<Character> Ch = new ArrayList<Character>();
         long s = hash;
         long h = 0L;
-        for (int i = 0; i < 100; i++){
+        int k = 0;
+        for (int i = 0; i < 11; i++){
             h = s % 23;
             s = ( s - h ) / 23;
             char ans = letters.charAt((int) h);
             System.out.println(ans);
+            Ch.add(ans);
+            k++;
             if (s == 7){
+                System.out.println(k);
+                Collections.reverse(Ch);
+                System.out.println(Ch);
                 break;
             }
         }
         return s;
+
     }
+
 
     public static long hash(String s){
         long h = 7;
