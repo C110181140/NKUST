4197d02f29eb125211bdc8723ca58a7f245c9aac
c110181140 <c110181140@nkust.edu.tw>
2021-11-30 16:26:45 +0800

    updata

--- Spider/src/main/java/Spider.java
+++ Spider/src/main/java/Spider.java
@@ -3,24 +3,28 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
 public class Spider<newsHeadlines> {
     public static void main(String[] args) {
-        try {
-            Document doc = Jsoup.connect("https://rule.nkust.edu.tw/").get();
-            System.out.println(doc.title());
-            Elements newsHeadlines = doc.select("#Dyn_2_2  div.mtitle  a");
-            for (Element headline : newsHeadlines) {
 
-                System.out.println(headline.attr("title"));
-                System.out.println(headline.absUrl("href"));
+
+    try{
+    Document doc =  Jsoup.connect("https://en.wikipedia.org/").get();
+    System.out.println(doc.title());
+
+    Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+
+            System.out.println(headline.attr("title"));
+            System.out.println(headline.absUrl("href"));
 
             }
-        } catch (Exception e) {
-
-            System.out.println(e);
-
         }
+
+    catch(Exception e) {
+
+        System.out.println(e);
+
+    }
 
     }
 }
