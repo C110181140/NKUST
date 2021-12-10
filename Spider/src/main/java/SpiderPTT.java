e526484111cf13c175178b93574616bcf71878d6
C110181140 <C110181140@nkust.edu.tw>
2021-12-10 21:17:14 +0800

    updata

--- Spider/src/main/java/SpiderPTT.java
+++ Spider/src/main/java/SpiderPTT.java
@@ -4,101 +4,104 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-import java.nio.file.LinkOption;
-import java.util.ArrayList;
-
 public class SpiderPTT {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
+
+
+
+
+        try{
 //        Document doc = Jsoup.connect("https://www.ptt.cc/bbs/index.html").get();
-//        Elements items = doc.getElementsByClass("b-ent");
+//        Elements Articletitle = doc.getElementsByClass("b-ent");
 //        System.out.println(doc.title());
 //
-//        for (Element item1 : items) {
+//        for (Element Atitle : Articletitle) {
 //
-//            String Num = item1.getElementsByClass("board-name").get(0).text();//"C_Chat";
-//            String title = item1.getElementsByClass("board-title").get(0).text();
-//            Document doc1 = Jsoup.connect("https://www.ptt.cc/bbs/" + Num + "/index.html").get();
-//            Elements newsHeadlines = doc.select("#main-container div.r-list-container.action-bar-margin.bbs-screen a");
-//            System.out.println("Board: " + Num);
-//            System.out.println("Article title: " + title);
+//            String item = Atitle.getElementsByClass("board-name").get(0).text();
+//            System.out.println("Board:" + item);
+//            String Text = Atitle.getElementsByClass("board-title").get(0).text();
+//            System.out.println("Article title:" + Text);
+//            String Url = Atitle.select("a").get(0).absUrl("href");
+//            Elements link = doc.select("#main-container div.r-list-container.action-bar-margin.bbs-screen a");
+////            System.out.println(Url);
+//            Document doc1 = Jsoup.connect("https://www.ptt.cc/bbs/" + item + "/index.html").get();
 //
-//            for (Element headline : newsHeadlines){
+//            for (Element links : link) {
 //
-//                Elements items1 = doc1.getElementsByClass("r-ent");
+//                Elements items = doc1.getElementsByClass("r-ent");
 //
-//                for (Element item2 : items1){
+//                for (Element item1 : items) {
 //
-//                    String Num1 = item2.getElementsByClass("title").get(0).text();
-//                    System.out.println(Num1);
-//                    Document doc2 = Jsoup.connect(headline.absUrl("href")).get();
-//                    Elements items2 = doc2.getElementsByClass("push");
+//                    String test = item1.select("a").get(0).absUrl("href");
+//                    Document doc2 = Jsoup.connect(links.absUrl("href")).get();
+//                    Elements item2 = doc2.getElementsByClass("push");
 //
-//                    for (Element item3 : items2){
+//                    for (Element item3 : item2) {
 //
-//                        String num2 = item3.getElementsByClass("push").get(0).getElementsByTag("span").get(2).text();
-//                        System.out.println("comment" + num2);
-//                        break;
+//                        String comment1 = item3.getElementsByClass("push").get(0).select("span").get(1).text();
+//                        String comment2 = item3.getElementsByClass("push").get(0).select("span").get(2).text();
+//                        System.out.println("comment:" + comment1 + comment2);
+//                          break;
 //
 //                    }
-//                    break;
+//                      break;
 //                }
-//                break;
+//                  break;
 //            }
-//
-//        }
+//              System.out.print("\n");
 
-
-//        try{
         Document doc = Jsoup.connect("https://www.ptt.cc/bbs/index.html").get();
-        Elements Articletitle = doc.getElementsByClass("b-ent");
         System.out.println(doc.title());
+        Elements Text1 = doc.getElementsByClass("b-ent");
 
-        for (Element Atitle : Articletitle) {
+        for (Element Texts : Text1) {
 
-            String item = Atitle.getElementsByClass("board-name").get(0).text();
-            System.out.println("Board:" + item);
-            String Text = Atitle.getElementsByClass("board-title").get(0).text();
-            System.out.println("Article title:" + Text);
-            String Url = Atitle.select("a").get(0).absUrl("href");
-            Elements link = doc.select("#main-container div.r-list-container.action-bar-margin.bbs-screen a");
-            System.out.println(Url);
-            Document doc1 = Jsoup.connect(Url).get();
+            String Atitle = Texts.getElementsByClass("board-name").get(0).text();
+            System.out.println("Board: " + Atitle);
+            String title = Texts.getElementsByClass("board-title").get(0).text();
+            System.out.println("Article title: " + title);
+            Document doc1 = Jsoup.connect("https://www.ptt.cc/bbs/" + Atitle + "/index.html").get();
+            Elements Url = doc1.select("#main-container div.r-list-container.action-bar-margin.bbs-screen a");
 
-            for (Element links : link) {
+            for (Element Urls : Url) {
 
-                Elements items = doc1.getElementsByClass("r-ent");
+                Elements Text = doc1.getElementsByClass("r-ent");
 
-                for (Element item1 : items) {
+                for (Element Url2 : Text) {
 
-                    String test = item1.select("a").get(0).absUrl("href");
-                    Document doc2 = Jsoup.connect(links.absUrl("href")).get();
-                    Elements item2 = doc2.getElementsByClass("push");
+                    Document doc2 = Jsoup.connect(Urls.absUrl("href")).get();
+                    Elements Push = doc2.getElementsByClass("push");
 
-                    for (Element item3 : item2) {
-
-                        String comment1 = item3.getElementsByClass("push").get(0).select("span").get(1).text();
-                        String comment2 = item3.getElementsByClass("push").get(0).select("span").get(2).text();
-                        System.out.println("comment:" + comment1 + comment2);
+                    for (Element Pushs : Push) {
+                        String Comment1 = Pushs.getElementsByClass("push").get(0).getElementsByTag("span").get(1).text();
+                        String Comment2 = Pushs.getElementsByClass("push").get(0).getElementsByTag("span").get(2).text();
+                        System.out.println("Comment:" + Comment1 + Comment2);
+                        break;
 
                     }
 
+                    break;
+
                 }
 
+                break;
+
             }
+
+            System.out.print("\n");
+
+        }
 
         }
 
 
+        catch(Exception e) {
 
+            System.out.println(e);
 
+        }
 
-//        catch(Exception e) {
-//
-//            System.out.println(e);
-//
-//        }
 
     }
 }
